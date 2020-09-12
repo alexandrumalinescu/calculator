1459a0ba9f986b129064ac2a33b7047a12ed1587
alexandrumalinescu <almalinescu@yahoo.com>
2020-09-12 12:09:59 +0300

    done

---
+++ app.js
@@ -0,0 +1,31 @@
+
+
+function calcul(event){
+  var firstNumber = parseInt(document.querySelector('#firstNumber').value);
+  var secondNumber = parseInt(document.querySelector('#secondNumber').value);
+  var operation = document.querySelector('#operation').value;
+  var result; 
+  
+  if(operation === "add")
+  {
+    result = firstNumber + secondNumber;
+  }
+  else if(operation === "min"){
+   result = firstNumber - secondNumber;
+  }
+  else if(operation === "multiply"){
+     result = firstNumber * secondNumber;
+  }
+  else if(operation === "divide"){
+     result = firstNumber / secondNumber;
+  }
+  document.getElementById("result").innerText = result;
+  
+}
+
+
+
+var playBtn = document.getElementById("Calculate");
+playBtn.addEventListener("click", calcul);
+
+ /// console.log(calcul());
